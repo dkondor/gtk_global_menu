6b848967c1f830eea81d28cd31b83e2f24fb3352
Kondor Dániel <kondor.dani@gmail.com>
2026-01-20 22:21:19 +0100

    update Python client

--- gtk_global_menu.py
+++ gtk_global_menu.py
@@ -2,271 +2,194 @@
 
 # A simple client for displaying global menus of compatible apps.
 
+import os, sys
+import socket
+import json
+from collections import deque
+
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('Gdk', '3.0')
 gi.require_version('GLib', '2.0')
-gi.require_version('GObject', '2.0')
 gi.require_version('DbusmenuGtk3', '0.4')
 
 from gi.repository import Gtk, Gdk, GLib, Gio, DbusmenuGtk3
 
-import socket
-import json as js
-import os
-from typing import Any, List, Dict, Optional
 
-def get_msg_template(method: str) -> Dict[str, Any]:
-    '''
-    Create generic message template for the given method call.
-    '''
-    message = {}
-    message["method"] = method
-    message["data"] = {}
-    return message
+class GlobalMenuClient:
+	
+	def __init__(self):
+		self.queue = deque()
+		self.conn = Gio.bus_get_sync(Gio.BusType.SESSION, None)
+		
+		wf_socket = os.getenv("WAYFIRE_SOCKET")
+		if wf_socket is None:
+			raise BaseException('No socket name set!')
+		
+		self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+		self.sock.connect(wf_socket)
+		self.ch = GLib.IOChannel.unix_new(self.sock.fileno())
+		self.ch.set_encoding(None)
+		self.ch.set_buffered(False)
+		
+		GLib.io_add_watch(self.ch, GLib.PRIORITY_HIGH, GLib.IO_IN | GLib.IO_HUP | GLib.IO_ERR, self.sock_event)
+		
+		self.send_msg({"method": "window-rules/events/watch", "data":
+			{"events": ["view-focused", "view-mapped", "view-unmapped"]}})
+		self.queue.append(None)
+		
+		self.active_view_id = None
+		self.entry = None
+		self.views = {}
+		self.self_id = None
+		self.menubtn = None
+		self.last_menu = None
+
+		self.win1 = Gtk.Window()
+		self.title = 'App menu test'
+		self.win1.set_title(self.title)
+		self.win1.set_size_request(400, 300)
+
+		box1 = Gtk.Box(orientation = Gtk.Orientation.VERTICAL)
+		box2 = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL)
+		lbl1 = Gtk.Label.new('Active app:  ')
+		self.entry = Gtk.Entry()
+		box2.add(lbl1)
+		box2.add(self.entry)
+		box1.add(box2)
+
+		btn = Gtk.MenuButton.new()
+		btn.set_label('Show menu')
+		self.menubtn = btn
+		btn.connect("clicked", self.show_menu_cb)
+		box1.add(btn)
+		
+		self.win1.add(box1)
+		self.win1.connect('destroy', Gtk.main_quit)
+		self.win1.show_all()
+
+	def setup_menu(self, props):
+		self.menubtn.insert_action_group("app", None)
+		self.menubtn.insert_action_group("win", None)
+		self.menubtn.insert_action_group("unity", None)
+		self.menubtn.set_menu_model(None)
+		self.menubtn.set_popup(None)
+		self.last_menu = None
+		
+		if props is None:
+			return
+		
+		if "gtk-shell-unique-bus-name" in props and "gtk-shell-menubar-path" in props:
+			name = props["gtk-shell-unique-bus-name"]
+			path = props["gtk-shell-menubar-path"]
+			win_path = props["gtk-shell-window-object-path"] if "gtk-shell-window-object-path" in props else None
+			app_path = props["gtk-shell-application-object-path"] if "gtk-shell-application-object-path" in props else None
+			
+			menumodel = Gio.DBusMenuModel.get(self.conn, name, path)
+			self.menubtn.set_menu_model(menumodel)
+			if app_path is not None:
+				app_actions = Gio.DBusActionGroup.get(self.conn, name, app_path)
+				self.menubtn.insert_action_group("app", app_actions)
+			if win_path is not None:
+				win_actions = Gio.DBusActionGroup.get(self.conn, name, win_path)
+				self.menubtn.insert_action_group("win", win_actions)
+			# fallback
+			unity_actions = Gio.DBusActionGroup.get(self.conn, name, path)
+			self.menubtn.insert_action_group("unity", unity_actions)
+		elif "kde-appmenu-service-name" in props and "kde-appmenu-object-path" in props:
+			name = props["kde-appmenu-service-name"]
+			path = props["kde-appmenu-object-path"]
+			if name is not None and path is not None:
+				menu = DbusmenuGtk3.Menu.new(name, path)
+				if menu:
+					menu.show_all()
+					self.menubtn.set_popup(menu)
+					self.last_menu = menu
+
+	def read_msg(self):
+		tmp1 = self.ch.read(4) # header
+		l2 = int.from_bytes(tmp1, byteorder = 'little')
+		tmp2 = self.ch.read(l2) # message
+		return json.loads(tmp2)
+	
+	def send_msg(self, msg):
+		data = json.dumps(msg).encode("utf8")
+		l1 = len(data)
+		len_bytes = l1.to_bytes(4, byteorder="little")
+		self.ch.write_chars(len_bytes, 4)
+		self.ch.write_chars(data, l1)
+	
+	def get_view_props(self, id1):
+		prop_names = ["kde-appmenu-service-name", "kde-appmenu-object-path",
+			"gtk-shell-app-menu-path", "gtk-shell-application-object-path",
+			"gtk-shell-menubar-path", "gtk-shell-unique-bus-name",
+			"gtk-shell-window-object-path"]
+		for p in prop_names:
+			self.send_msg({"method": "window-rules/get-view-property",
+				"data": {"id": id1, "property": p}})
+			self.queue.append((id1, p))
+
+	def sock_event(self, ch, cond):
+		msg = self.read_msg()
+		if msg is None:
+			return False # should not happen, will likely throw an exception instead
+		if "event" in msg:
+			if msg["event"] == "view-focused":
+				if ("view" in msg) and (msg["view"] is not None):
+					id1 = msg["view"]["id"]
+					if self.self_id and (id1 != self.self_id) and msg["view"]["type"] == "toplevel" and (id1 != self.active_view_id):
+						self.entry.set_text(msg["view"]["title"])
+						if id1 in self.views:
+							self.setup_menu(self.views[id1])
+						else:
+							self.setup_menu(None)
+							self.get_view_props(id1)
+						self.active_view_id = id1
+			elif msg["event"] == "view-mapped":
+				if ("view" in msg) and (msg["view"] is not None):
+					id1 = msg["view"]["id"]
+					if msg["view"]["title"] == self.title:
+						self.self_id = id1
+					if msg["view"]["type"] == "toplevel":
+						self.get_view_props(id1)
+			elif msg["event"] == "view-unmapped":
+				if ("view" in msg) and (msg["view"] is not None):
+					id1 = msg["view"]["id"]
+					if id1 in self.views:
+						del self.views[id1]
+					if id1 == self.active_view_id:
+						self.setup_menu(None)
+						self.active_view_id = None
+		else:
+			# this should be a reply to one of our queries
+			x = self.queue.popleft()
+			if x is not None:
+				view_id = x[0]
+				prop_name = x[1]
+				if not view_id in self.views:
+					self.views[view_id] = {}
+				if "value" in msg:
+					self.views[view_id][prop_name] = msg["value"]
+				if prop_name == "gtk-shell-window-object-path":
+					# last one, re-check
+					if view_id == self.active_view_id:
+						self.setup_menu(self.views[view_id])
+		return True
+
+	def show_menu_cb(self, btn):
+		if self.last_menu:
+			self.last_menu.popup_at_widget(btn, Gdk.Gravity.SOUTH_WEST, Gdk.Gravity.NORTH_WEST, None)
 
 
-class WayfireGioSocket:
-    '''
-    Minimal adaptation of WayfireSocket to better work with GLib.MainLoop
-    '''
-    def __init__(self, socket_name: str | None=None, allow_manual_search=False):
-        if socket_name is None:
-            socket_name = os.getenv("WAYFIRE_SOCKET")
+def main(args):
+	Gtk.init()
+	cl = GlobalMenuClient()
+	Gtk.main()
+	return 0
 
-        self.socket_name = None
-        self.pending_events = []
-
-        if socket_name is None and allow_manual_search:
-            # the last item is the most recent socket file
-            socket_list = sorted(
-                [
-                    os.path.join("/tmp", i)
-                    for i in os.listdir("/tmp")
-                    if "wayfire-wayland" in i
-                ]
-            )
-
-            for candidate in socket_list:
-                try:
-                    self.connect_client(candidate)
-                    self.socket_name = candidate
-                    break
-                except Exception:
-                    pass
-
-        elif socket_name is not None:
-            self.connect_client(socket_name)
-            self.socket_name = socket_name
-
-        if self.socket_name is None:
-            raise Exception("Failed to find a suitable Wayfire socket!")
-
-    def connect_client(self, socket_name):
-        self.client = Gio.Socket.new(Gio.SocketFamily.UNIX, Gio.SocketType.STREAM, 0)
-        # self.client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-        self.client.connect(Gio.UnixSocketAddress.new(socket_name))
-        # self.client.setblocking(False)
-
-    def close(self):
-        self.client.close()
-
-    def read_exact(self, n: int):
-        response = bytes()
-        while n > 0:
-            try:
-                read_this_time = self.client.receive_bytes(n, -1, None).get_data()
-            except BlockingIOError:
-                return None
-            if read_this_time is None:
-                return None
-            n -= len(read_this_time)
-            response += read_this_time
-
-        return response
-
-    def read_message(self):
-        tmp1 = self.read_exact(4)
-        if tmp1 is None:
-            return None
-        rlen = int.from_bytes(tmp1, byteorder="little")
-        response_message = self.read_exact(rlen)
-        if response_message is None:
-            raise Exception("Error reading data from socket!\n")
-        response = js.loads(response_message)
-
-        if "error" in response and response["error"] == "No such method found!":
-            raise Exception(f"Method {response['method']} is not available. \
-                    Please ensure that the '{self._wayfire_plugin_from_method(response['method'])}' Wayfire plugin is enabled. \
-                    Once enabled, restart Wayfire to ensure that ipc was correctly loaded.")
-        elif "error" in response:
-            raise Exception(response["error"])
-        return response
-
-    def read_next_event(self):
-        if self.pending_events:
-            return self.pending_events.pop(0)
-        return self.read_message()
-
-    def send_json(self, msg):
-        if 'method' not in msg:
-            raise Exception("Malformed json request: missing method!")
-
-        data = js.dumps(msg).encode("utf8")
-        header = len(data).to_bytes(4, byteorder="little")
-        self.client.send(header)
-        self.client.send(data)
-
-        while True:
-            response = self.read_message()
-            if 'event' in response:
-                self.pending_events.append(response)
-                continue
-
-            return response
-
-    def watch(self, events: List[str] | None = None):
-        """
-        Subscribes to specific events or all events for monitoring.
-
-        This method sends a request to start watching for specified events. If no events are provided,
-        it will subscribe to all available events. 
-
-        Args:
-            events (List[str] | None): A list of event names to watch. If `None`, subscribes to all events.
-
-        Returns:
-            The response from sending the JSON message, which confirms the subscription to the specified
-            events.
-        """
-        method = "window-rules/events/watch"
-        message = get_msg_template(method)
-        if events is not None:
-            message["data"]["events"] = events
-        return self.send_json(message)
+if __name__ == '__main__':
+	sys.exit(main(sys.argv[1:]))
 
 
-
-sock = WayfireGioSocket()
-sock.watch(["view-focused"])
-
-active_view_id = None
-entry = None
-view_ids = {}
-self_id = None
-conn = None
-menubtn = None
-last_menu = None
-
-def sock_event(ch, cond):
-    global self_id
-    global active_view_id
-    global conn
-    global last_menu
-    print("in sock_event")
-    msg = sock.read_next_event()
-    if msg is None:
-        return False
-    if "event" in msg:
-        print(msg["event"].ljust(25), end = ": ")
-        if "view" in msg:
-            if (msg["view"] is not None):
-                print(msg["view"]["app-id"], end = " - ")
-                print(msg["view"]["id"])
-                view_ids[msg["view"]["id"]] = msg["view"]["app-id"]
-                if msg["view"]["app-id"] == "gtk_global_menu.py":
-                    self_id = msg["view"]["id"]
-            else:
-                print('')
-        else:
-            print('')
-        
-        if msg["event"] == "view-focused":
-            
-            if ("view" in msg) and (msg["view"] is not None):
-                id1 = msg["view"]["id"]
-                if self_id and (id1 != self_id) and msg["view"]["type"] == "toplevel":
-                    entry.set_text(msg["view"]["title"])
-                    
-                    menubtn.insert_action_group("app", None)
-                    menubtn.insert_action_group("win", None)
-                    menubtn.insert_action_group("unity", None)
-                    menubtn.set_menu_model(None)
-                    menubtn.set_popup(None)
-                    last_menu = None
-                    
-                    if "gtk_dbus_properties" in msg["view"]:
-                        props = msg["view"]["gtk_dbus_properties"]
-                        name = props["unique_bus_name"]
-                        path = props["menubar_path"]
-                        win_path = props["window_object_path"]
-                        app_path = props["application_object_path"]
-                        
-                        if name is not None and path is not None:
-                            if conn is None:
-                                conn = Gio.bus_get_sync(Gio.BusType.SESSION, None)
-                            menumodel = Gio.DBusMenuModel.get(conn, name, path)
-                            menubtn.set_menu_model(menumodel)
-                            if app_path is not None:
-                                app_actions = Gio.DBusActionGroup.get(conn, name, app_path)
-                                menubtn.insert_action_group("app", app_actions)
-                            if win_path is not None:
-                                win_actions = Gio.DBusActionGroup.get(conn, name, win_path)
-                                menubtn.insert_action_group("win", win_actions)
-                            # fallback
-                            unity_actions = Gio.DBusActionGroup.get(conn, name, path)
-                            menubtn.insert_action_group("unity", unity_actions)
-                    elif "kde_appmenu_properties" in msg["view"]:
-                        props = msg["view"]["kde_appmenu_properties"]
-                        name = props["service_name"]
-                        path = props["object_path"]
-                        if name is not None and path is not None:
-                            menu = DbusmenuGtk3.Menu.new(name, path)
-                            if menu:
-                                last_menu = menu
-                                last_menu.show_all()
-                                menubtn.set_popup(last_menu)
-                            
-                                
-#            else:
-#                entry.set_text("")
-#                    if not have_new_menu:
-#                        menubtn.insert_action_group("app", None)
-#                        menubtn.insert_action_group("win", None)
-#                        menubtn.set_menu_model(None)
-    return True
-
-def show_menu_cb(btn):
-    if last_menu:
-        last_menu.popup_at_widget(btn, Gdk.Gravity.SOUTH_WEST, Gdk.Gravity.NORTH_WEST, None)
-
-Gtk.init()
-
-win1 = Gtk.Window()
-win1.set_title('App menu test')
-
-box1 = Gtk.Box(orientation = Gtk.Orientation.VERTICAL)
-box2 = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL)
-lbl1 = Gtk.Label.new('Active app:  ')
-entry = Gtk.Entry()
-box2.add(lbl1)
-box2.add(entry)
-box1.add(box2)
-
-win1.set_size_request(400, 300)
-btn = Gtk.MenuButton.new()
-btn.set_label('Show menu')
-menubtn = btn
-btn.connect("clicked", show_menu_cb)
-box1.add(btn)
-win1.add(box1)
-
-win1.connect('destroy', Gtk.main_quit)
-win1.show_all()
-
-ch = GLib.IOChannel.unix_new(sock.client.get_fd())
-ch.add_watch(GLib.IO_IN, sock_event, priority = GLib.PRIORITY_HIGH)
-
-Gtk.main()
 
 
